c7707cabde93470bda93e47e54a153c0c27767c8
bot <awcbaker@gmail.com>
2017-09-07 20:08:57 -0600

    Added help and banana commands, as well as made sure Banana-Bot does not respond to itself.

--- bananabot.py
+++ bananabot.py
@@ -21,23 +21,25 @@ async def on_ready():
 #Anytime a message is added, check to see if it's important
 @client.event
 async def on_message(message):
-    #If !quit, the bot will exit if the message author is the SUPER_ADMIN
-    if message.content.startswith("!quit"):
-        #Check to see if I'm the one that told it to quit
-        if (str(message.author) == SUPER_ADMIN):
-            await client.send_message(message.channel, "Bye!")
-            await client.logout()
-            await client.close()
-            return
+    #Make sure BananaBot isn't the one sending the message
+    if (client.user != message.author):
+        #If !quit, the bot will exit if the message author is the SUPER_ADMIN
+        if message.content.startswith("!quit"):
+            #Check to see if I'm the one that told it to quit
+            if (str(message.author) == SUPER_ADMIN):
+                await client.send_message(message.channel, "Bye!")
+                await client.logout()
+                await client.close()
+                return
 
-        else:
-            author = str(message.author).split("#")
-            author = author[0]
-            await client.send_message(message.channel, "You can't make me quit, " + author)
+            else:
+                author = str(message.author).split("#")
+                author = author[0]
+                await client.send_message(message.channel, "You can't make me quit, " + author)
 
-    #Other commands go here
-    elif message.content.startswith("!"):
-        await messages.message_check(client, message) #Check messages.py for more detail
+        #Other commands go here
+        elif message.content.startswith("!"):
+            await messages.message_check(client, message) #Check messages.py for more detail
 
 #This line goes last, contains the token that let's the Discord servers know which bot is doing what
 with open("token.txt", "r") as token:
--- messages.py
+++ messages.py
@@ -1,9 +1,21 @@
 import discord
 import asyncio
 
+commands = {"!help": "Print these help messages.",
+            "!test": "Print a test message.",
+            "!sleep": "Sleep for 5 seconds.",
+            "!rule34": "Post a sexy image ;D",
+            "!waifu": "Call someone's waifu shit.",
+            "!banana": "Ooh! Banana!"}
+
 async def message_check(client, message):
+    #Print commands that this bot knows
+    if (message.content.startswith("!help")):
+        for key,value in commands.items():
+            await client.send_message(message.channel, key + ": " + value)
+
     #Print a test message
-    if (message.content.startswith("!test")):
+    elif (message.content.startswith("!test")):
         await client.send_message(message.channel, "This has been a test of your local test bot.")
 
     #Have it sleep for 5 seconds
@@ -19,10 +31,15 @@ async def message_check(client, message):
         #And call them a perv
         await client.send_message(message.channel, author + " is a fuckin' perv.", tts=True)
 
+    #Your waifu is shit
     elif (message.content.startswith("!waifu")):
         await client.send_message(message.channel, ":poop: YOUR WAIFU IS SHIT! :poop:")
         await asyncio.sleep(2)
         await client.send_message(message.channel, "SHIIIIITTTTTT!")
+
+    #Ooh! Banana!
+    elif (message.content.startswith("!banana")):
+        await client.send_message(message.channel, "https://www.youtube.com/watch?v=z8WvSGNEV24")
 
     return
 
