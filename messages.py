ea99d2253e7076ffebb3953d2043978157eae6b7
videogames4all <AWCBaker@gmail.com>
2017-09-09 16:18:55 -0600

    Added the penis command.

--- messages.py
+++ messages.py
@@ -1,12 +1,17 @@
+#For discord
 import discord
 import asyncio
+
+#For random numbers
+import random
 
 commands = {"!help": "Print these help messages.",
             "!test": "Print a test message.",
             "!sleep": "Sleep for 5 seconds.",
             "!rule34": "Post a sexy image ;D",
             "!waifu": "Call someone's waifu shit.",
-            "!banana": "Ooh! Banana!"}
+            "!banana": "Ooh! Banana!",
+            "!penis, !dong, !dick, !schlong, !johnson, !bird, !weiner": "PENIS!"}
 
 async def message_check(client, message):
     #Print commands that this bot knows
@@ -40,6 +45,12 @@ async def message_check(client, message):
     #Ooh! Banana!
     elif (message.content.startswith("!banana")):
         await client.send_message(message.channel, "https://www.youtube.com/watch?v=z8WvSGNEV24")
+
+    #Penis
+    elif (message.content.startswith("!penis")) or (message.content.startswith("!dong")) or (message.content.startswith("!dick")) or (message.content.startswith("!schlong")) or (message.content.startswith("!johnson")) or (message.content.startswith("!bird")) or (message.content.startswith("!weiner")):
+        penis_length = random.randint(2,20)
+        penis = "8" + "=" * penis_length + "D"
+        await client.send_message(message.channel, penis)
 
     return
 
