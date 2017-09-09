8f775f9e09eaddb212e1ae95ad3584da8bc769db
videogames4all <AWCBaker@gmail.com>
2017-09-09 17:06:12 -0600

    Message testing, part 2

--- messages.py
+++ messages.py
@@ -11,26 +11,33 @@ commands = {"!help": "Print these help messages.",
             "!rule34": "Post a sexy image ;D",
             "!waifu": "Call someone's waifu shit.",
             "!banana": "Ooh! Banana!",
-            "!penis, !dong, !dick, !schlong, !johnson, !bird, !weiner": "PENIS!"}
+            "!penis, !dong, !dick, !schlong, !johnson, !bird, !weiner, !cock": "PENIS!"}
+
+penis_commands = ["!penis", "!dong", "!dick", "!schlong", "!johnson", "!bird", "!weiner", "!cock"]
 
 async def message_check(client, message):
-    print(message.content)
+    full_message = message.content.split(" ")
+    command = full_message[0]
+    args = None
+    if (len(full_message) > 1):
+        args = full_message[1:]
+
     #Print commands that this bot knows
-    if (message.content.startswith("!help")):
+    if (command == "!help"):
         for key,value in commands.items():
             await client.send_message(message.channel, key + ": " + value)
 
     #Print a test message
-    elif (message.content.startswith("!test")):
+    elif (command == "!test"):
         await client.send_message(message.channel, "This has been a test of your local test bot.")
 
     #Have it sleep for 5 seconds
-    elif (message.content.startswith("!sleep")):
+    elif (command == "!sleep"):
         await asyncio.sleep(5)
         await client.send_message(message.channel, "Done sleeping.")
 
     #"Post porn", except just outright call the user a pervert
-    elif (message.content.startswith("!rule34")):
+    elif (command == "!rule34"):
         #Pull apart the author of the message
         author = str(message.author).split("#")
         author = author[0]
@@ -38,17 +45,17 @@ async def message_check(client, message):
         await client.send_message(message.channel, author + " is a fuckin' perv.", tts=True)
 
     #Your waifu is shit
-    elif (message.content.startswith("!waifu")):
+    elif (command == "!waifu"):
         await client.send_message(message.channel, ":poop: YOUR WAIFU IS SHIT! :poop:")
         await asyncio.sleep(2)
         await client.send_message(message.channel, "SHIIIIITTTTTT!")
 
     #Ooh! Banana!
-    elif (message.content.startswith("!banana")):
+    elif (command == "!banana"):
         await client.send_message(message.channel, "https://www.youtube.com/watch?v=z8WvSGNEV24")
 
     #Penis
-    elif (message.content.startswith("!penis")) or (message.content.startswith("!dong")) or (message.content.startswith("!dick")) or (message.content.startswith("!schlong")) or (message.content.startswith("!johnson")) or (message.content.startswith("!bird")) or (message.content.startswith("!weiner")):
+    elif (command in penis_commands):
         penis_length = random.randint(2,20)
         penis = "8" + "=" * penis_length + "D"
         await client.send_message(message.channel, penis)
