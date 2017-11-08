b0f06d96a71e3c47b1d5788405870ed30309f4c0
bot <awcbaker@gmail.com>
2017-11-07 20:55:25 -0700

    Fixed Pokemon command, using a wrapper that works with pokeapi.co

--- pokemon.py
+++ pokemon.py
@@ -2,36 +2,20 @@
 import discord
 import asyncio
 
-#Pull from pokeapi.co
-import http.client
-import json
-import requests
-
 #Pull a random Pokemon if no args are passed
 import random
 
+#Pokeapi just got a lot easier
+import pokebase
+
 #Look at the info passed and grab info on the Pokemon wanted
 async def pokemon_get(client, message, args):
-    #Open a connection to pokeapi.co
-    conn = http.client.HTTPConnection("pokeapi.co")
-    payload = ""
-    conn.request("GET", "/api/v2/pokemon/bulbasaur/", payload)
-    res = conn.getresponse()
-    data = res.read()
-    obj = json.loads(data.decode("utf-8"))
-
-    pokemon = None
+    poke_num = None
     if (args == None):
-        pokemon = random.randint(1, 802)
+        poke_num = random.randint(1, 802)
 
     else:
-        pokemon = args[0]
+        poke_num = args[0]
 
-    url = "http://pokeapi.co/api/v2/pokemon/" + str(pokemon) + "/"
-    response = requests.request("GET", url, data=payload)
-    data = response.json()
-    output = data['name'].title() + ": #" + str(data['id'])
-    print(output)
-    await client.send_message(message.channel, output)
-
-    conn.close()
+    pokemon = pokebase.pokemon(poke_num)
+    await client.send_message(message.channel, pokemon.name)
