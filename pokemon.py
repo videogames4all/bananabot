067946693e5912c95932b3dad568ed095affaf55
bot <awcbaker@gmail.com>
2017-11-12 15:16:03 -0700

    Added Pokemon types to show up.

--- pokemon.py
+++ pokemon.py
@@ -16,15 +16,26 @@ import sys
 async def pokemon_get(client, message, args):
     poke_num = None
     if (args == None):
-        poke_num = random.randint(1, 802)
+        poke_num = str(random.randint(1, 802))
 
     else:
         poke_num = args[0]
 
     try:
         pokemon = pokebase.pokemon(poke_num.lower())
-        poke_string = str(pokemon.id) + ": " + pokemon.name.title()
+        poke_string = str(pokemon.id) + ": " + pokemon.name.title() + " - "
+        #See how many types there are for the pulled Pokemon, and append as needed.
+        type_string = ""
+        if (len(pokemon.types) == 2):
+            type_string = str(pokemon.types[0].type).title() + "/" + str(pokemon.types[1].type).title() + " type."
+        else:
+            type_string = str(pokemon.types[0].type).title() + " type."
+
+        poke_string = poke_string + type_string
+
+        #Send the message
         await client.send_message(message.channel, poke_string)
 
     except:
+        traceback.print_exc()
         await client.send_message(message.channel, sys.exc_info()[0])
