ef0206f351afa1e4924011879bc2028296503ccc
bot <awcbaker@gmail.com>
2017-11-12 14:55:21 -0700

    Fixed up Pokemon command.

--- pokemon.py
+++ pokemon.py
@@ -8,6 +8,10 @@ import random
 #Pokeapi just got a lot easier
 import pokebase
 
+#For exceptions
+import traceback
+import sys
+
 #Look at the info passed and grab info on the Pokemon wanted
 async def pokemon_get(client, message, args):
     poke_num = None
@@ -17,5 +21,10 @@ async def pokemon_get(client, message, args):
     else:
         poke_num = args[0]
 
-    pokemon = pokebase.pokemon(poke_num)
-    await client.send_message(message.channel, pokemon.name)
+    try:
+        pokemon = pokebase.pokemon(poke_num.lower())
+        poke_string = str(pokemon.id) + ": " + pokemon.name.title()
+        await client.send_message(message.channel, poke_string)
+
+    except:
+        await client.send_message(message.channel, sys.exc_info()[0])
