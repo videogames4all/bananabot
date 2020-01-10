d9cbb97273119998e632f4b5cf6bc6d3daf1d664
Andrew Baker <awcbaker@gmail.com>
2020-01-09 20:04:43 -0600

    Starting LCR and Water. Just with comment files.

---
+++ lcr.py
@@ -0,0 +1,6 @@
+#A drinking game version of Left-Center-Right
+#Must have 3 players minimum, each player starts with 3 tokens/drinks
+#Allow for self-drinking, a pot that the winner distributes among people
+#1 game per Server
+#People can drop mid-game, remaining drinks go to center
+#Allow game ending in the middle in case of anything
